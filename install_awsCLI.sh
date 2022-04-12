420b11e0e7fc25627eb431c509388ac8edc99b62
Ricardo Coelho <ricardo.coelho@sgp.uerj.br>
2022-04-12 10:34:47 -0300

    feat: Script install AWS CLI

---
+++ install_awsCLI.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#ref: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+./aws/install
