657695a40e78aae862e6bb68996fc9c9320931da
Ricardo Coelho <ricardo.rcj@gmail.com>
2022-04-21 15:57:36 -0300

    refactor: Multiple aws instances. Without ansible provider

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -14,30 +14,24 @@ provider "aws" {
   region  = var.location
 }
 
+locals {
+  list_instance_count = length(flatten([for i in var.vms: range(i["instance_count"])]))
+  list_ami_id = [for i in var.vms: i["ami_id"]]
+  list_instance_type = [for i in var.vms: i["instance_type"]]
+  list_key_name = [for i in var.vms: i["key_name"]]
+  list_name = tolist(keys(var.vms))
+}
+
 resource "aws_instance" "app_server" {
-  count         = var.instance_count
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.ssh_key_name
+  count         = local.list_instance_count
+  ami           = element(local.list_ami_id, count.index +1 )
+  instance_type = element(local.list_instance_type, count.index +1 )
+  key_name      = element(local.list_key_name, count.index +1 ) 
 
   tags = {
     # The count.index allows you to launch a resource 
     # starting with the distinct index number 0 and corresponding to this instance.
-    Name = "srv${count.index}"
-  }
-}
-
-resource "null_resource" "ProvisionRemoteHostsIpToAnsibleHosts" {
-  count = "${var.instance_count}"
-  connection {
-    type        = "ssh"
-    user        = var.ssh_user_name
-    private_key = file(pathexpand(var.ssh_key_path))
-    host        = "${element(aws_instance.app_server.*.public_ip, count.index)}"
-  }
-
-  provisioner "local-exec" { 
-    command = "sleep 60; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user_name} -i '${element(aws_instance.app_server.*.public_ip, count.index)},' --private-key ${var.ssh_key_path} play.yaml" 
+    Name = element(local.list_name, count.index +1 ) 
   }
 }
 
--- learn-terraform-aws-instance/variables.tf
+++ learn-terraform-aws-instance/variables.tf
@@ -1,46 +1,17 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "ExampleAppServerInstance"
-}
-
-variable "resource_group_name" {
-  description = "Default resource group name that the network will be created in."
-  type        = list(string)
-  default     = ["srv1", "srv2"]
-}
-
-variable "location" {
-  description = "The location/region where the core network will be created."
-  default     = "sa-east-1"
-}
-
-variable "instance_count" {
-  description = "number of instances"
-  default     = 2
-}
-
-variable "ami_id" {
-  description = "Code of instances System"
-  default     = "ami-0c2485d67d416fc4f"
-}
-
-variable "instance_type" {
-  description = "Code of instances type"
-  default     = "t2.micro"
-}
-
-variable "ssh_key_name" {
-  description = "AWS EC2 ssh key name"
-  default     = "acesso"
-}
-
-variable "ssh_user_name" {
-  description = "SSH user"
-  default     = "ec2-user"
-}
-
-variable "ssh_key_path" {
-  description = "Local ssh key"
-  default     = "~/Downloads/acesso.pem"
+variable "vms" {
+  #type = map(string)
+  default = {
+    "srv01" = {
+      "ami_id" = "ami-0c2485d67d416fc4f"
+      "instance_type" = "t2.micro"
+      "key_name" = "acesso"
+      "instance_count" = 1
+    },
+    "srv02" = {
+      "ami_id" = "ami-0c2485d67d416fc4f"
+      "instance_type" = "t2.micro"
+      "key_name" = "acesso"
+      "instance_count" = 2
+    },      
+  }
 }
